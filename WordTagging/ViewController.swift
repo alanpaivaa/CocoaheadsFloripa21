b943fefbe1296a629da097e5c5978113353d2d4e
Alan Jeferson <alan.jeferson@arctouch.com>
2019-03-31 14:08:00 -0300

    Setup initial project

---
+++ WordTagging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordTagging
+//
+//  Created by Alan Jeferson on 3/31/19.
+//  Copyright © 2019 Alan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
