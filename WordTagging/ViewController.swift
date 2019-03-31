42b02ea32f9b744a5595f0bf2c20c313c3307767
Alan Jeferson <alan.jeferson@arctouch.com>
2019-03-31 15:30:09 -0300

    Add word tagging interface

--- WordTagging/ViewController.swift
+++ WordTagging/ViewController.swift
@@ -8,13 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+  
+  private var words: [(title: String, tokens: [String])] = []
+  private let tagger = CustomNameTagger()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    words = [(title: "Some", tokens: ["a", "b", "c"])]
+    tableView.reloadData()
   }
-
-
+  
+  @IBAction func didTouchPlusButton(_ sender: Any) {
+    let alert = UIAlertController(title: "New sentence",
+                                  message: "Type the sentence bellow",
+                                  preferredStyle: .alert)
+    alert.addTextField(configurationHandler: nil)
+    let okAction = UIAlertAction(title: "Ok", style: .default) { [unowned self] _ in
+      guard let sent = alert.textFields?.first?.text,
+        !sent.isEmpty else { return }
+      self.tag(sent: sent)
+    }
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    
+    alert.addAction(okAction)
+    alert.addAction(cancelAction)
+    present(alert, animated: true, completion: nil)
+  }
+  
+  func tag(sent: String) {
+    let tags = tagger.tag(sent: sent)
+    let maleTokens = tags.filter { $0.1 == "MALE" }.map { $0.0 }
+    let femaleTokens = tags.filter { $0.1 == "FEMALE" }.map { $0.0 }
+    let otherTokens = tags.filter { $0.1 == "NONE" }.map { $0.0 }
+    words = [
+      (title: "Female", tokens: femaleTokens),
+      (title: "Male", tokens: maleTokens),
+      (title: "Other", tokens: otherTokens)
+    ]
+    tableView.reloadData()
+  }
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return words.count
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return words[section].tokens.count
+  }
+  
+  override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return words[section].title
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+    cell.textLabel?.text = words[indexPath.section].tokens[indexPath.row]
+    return cell
+  }
 }
 
